eae076d34d1679ca3dead1cde038af69f103ac4f
sangrim shin <sangrimshin2020@gmail.com>
2026-02-23 20:49:33 +0900

    [feat] slide down animation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,17 @@
-import { useState, useEffect } from 'react';
-import './App.css'
-import api from '../util/api';
-import type { WeatherResponse } from '../type/WeatherResponse';
-import DetailInfo from './app/DetailInfo';
+import { useState, useEffect } from "react";
+import "./App.css";
+import api from "../util/api";
+import type { WeatherResponse } from "../type/WeatherResponse";
+import DetailInfo from "./app/DetailInfo";
 
 function App() {
   const [weather, setWeather] = useState<WeatherResponse | null>(null);
   const [isShowDetail, setIsShowDetail] = useState(false);
+  const [isExiting, setIsExiting] = useState(false);
 
   useEffect(() => {
     const fetchWeather = async () => {
-      const data = await api.getWeather('Seoul');
+      const data = await api.getWeather("Seoul");
       console.log(data);
       setWeather(data);
     };
@@ -18,31 +19,51 @@ function App() {
     // 의존성 배열이 비어있으면 컴포넌트가 마운트될 때 한 번만 실행
   }, []);
 
-
   const showDetail = () => {
-    console.log('showDetail');
-    setIsShowDetail(prev => !prev);
+    console.log("showDetail");
+    if (isShowDetail) {
+      setIsShowDetail(false);
+      setIsExiting(true);
+      setTimeout(() => {
+        setIsExiting(false);
+      }, 1000);
+    } else {
+      setIsShowDetail(true);
+    }
   };
   return (
-    <div className='app'>
-      <div className='title'>Weather App</div>
+    <div className="app">
+      <div className="title">Weather App</div>
       <div>
         <div>나의 위치</div>
-        <div className='city-name'>서울</div>
+        <div className="city-name">서울</div>
       </div>
-      <div className='summary-info'>
-        <p className='temperature'>{weather?.main.temp}°C</p>
-        <p className='feels-like'>체감 {weather?.main.feels_like}°C</p>
-        <p className='temp-min'>최저 {weather?.main.temp_min}°C</p>
-        <p className='temp-max'>최고 {weather?.main.temp_max}°C</p>
+      <div className="summary-info">
+        <p className="temperature">{weather?.main.temp}°C</p>
+        <p className="feels-like">체감 {weather?.main.feels_like}°C</p>
+        <p className="temp-min">최저 {weather?.main.temp_min}°C</p>
+        <p className="temp-max">최고 {weather?.main.temp_max}°C</p>
       </div>
-      <div className='detail-info-button card' onClick={() => { showDetail() }} > {isShowDetail ? 'show less information' : 'show detail information'}</div>
 
-      {isShowDetail && (
-        <DetailInfo weather={weather} />
-      )
-      }
+      {!isShowDetail && (
+        <div
+          className="detail-info-button card"
+          onClick={() => {
+            showDetail();
+          }}
+        >
+          {" "}
+          {"show detail information"}
+        </div>
+      )}
 
+      {(isShowDetail || isExiting) && (
+        <DetailInfo
+          weather={weather}
+          isExiting={isExiting}
+          showDetail={showDetail}
+        />
+      )}
     </div>
   );
 }
--- src/components/app/DetailInfo.tsx
+++ src/components/app/DetailInfo.tsx
@@ -1,32 +1,43 @@
 import type { WeatherResponse } from "../../type/WeatherResponse";
 import "./DetailInfo.css";
 
-export default function DetailInfo({ weather }: { weather: WeatherResponse | null }) {
-    return (
-        <div className="detail-info card">
-            <p>날씨 ID: {weather?.weather[0].id}</p>
-            <p>날씨 상태: {weather?.weather[0].main}</p>
-            <p>날씨 설명: {weather?.weather[0].description}</p>
-            <p>날씨 아이콘: {weather?.weather[0].icon}</p>
-            <p>가시성: {weather?.visibility}m</p>
+export default function DetailInfo({
+  weather,
+  isExiting,
+  showDetail,
+}: {
+  weather: WeatherResponse | null;
+  isExiting: boolean;
+  showDetail: () => void;
+}) {
+  return (
+    <div className={`detail-info card ${isExiting ? "exiting" : ""}`}>
+      <div onClick={showDetail} className="detail-info-header card">
+        close detail information
+      </div>
+      <p>날씨 ID: {weather?.weather[0].id}</p>
+      <p>날씨 상태: {weather?.weather[0].main}</p>
+      <p>날씨 설명: {weather?.weather[0].description}</p>
+      <p>날씨 아이콘: {weather?.weather[0].icon}</p>
+      <p>가시성: {weather?.visibility}m</p>
 
-            <p>바람 속도: {weather?.wind.speed} m/s</p>
-            <p>바람 방향: {weather?.wind.deg}°</p>
+      <p>바람 속도: {weather?.wind.speed} m/s</p>
+      <p>바람 방향: {weather?.wind.deg}°</p>
 
-            <p>구름 정도: {weather?.clouds.all}%</p>
+      <p>구름 정도: {weather?.clouds.all}%</p>
 
-            <p>날짜 및 시간: {weather?.dt}</p>
+      <p>날짜 및 시간: {weather?.dt}</p>
 
-            <p>시스템 유형: {weather?.sys.type}</p>
-            <p>시스템 ID: {weather?.sys.id}</p>
-            <p>국가: {weather?.sys.country}</p>
-            <p>일출 시간: {weather?.sys.sunrise}</p>
-            <p>일몰 시간: {weather?.sys.sunset}</p>
+      <p>시스템 유형: {weather?.sys.type}</p>
+      <p>시스템 ID: {weather?.sys.id}</p>
+      <p>국가: {weather?.sys.country}</p>
+      <p>일출 시간: {weather?.sys.sunrise}</p>
+      <p>일몰 시간: {weather?.sys.sunset}</p>
 
-            <p>시간대: {weather?.timezone}</p>
-            <p>도시 ID: {weather?.id}</p>
-            <p>도시 이름: {weather?.name}</p>
-            <p>코드: {weather?.cod}</p>
-        </div>
-    );
+      <p>시간대: {weather?.timezone}</p>
+      <p>도시 ID: {weather?.id}</p>
+      <p>도시 이름: {weather?.name}</p>
+      <p>코드: {weather?.cod}</p>
+    </div>
+  );
 }
