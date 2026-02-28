7959873b5e914b0ee19df627d82ac510998adf0c
sangrim shin <sangrimshin2020@gamil.com>
2026-02-28 22:52:23 +0900

    [css]delete button 위치 시키기

--- src/components/app/menu/menuAddCity/citySelect/CitySelectList.tsx
+++ src/components/app/menu/menuAddCity/citySelect/CitySelectList.tsx
@@ -11,6 +11,9 @@ export default function CitySelectList({
   setSelectedCityIndex: React.Dispatch<React.SetStateAction<number>>;
   setIsShowCitySelect: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
+  const handleDelete = (index: number) => {
+    console.log(index);
+  };
   const handleClick = (index: number) => {
     console.log(index);
     setSelectedCityIndex(index);
@@ -19,48 +22,56 @@ export default function CitySelectList({
   return (
     <>
       {weathers.map((weather, index) => (
-        <div
-          className="city-select-item card"
-          key={index}
-          onClick={() => handleClick(index)}
-        >
-          <div className="city-select-item-header">
-            <div className="city-select-item-header-left">
-              <div className="city-select-item-name">{weather.name}</div>
-              <div className="city-select-item-time">
-                {getLocalTime(weather.dt, weather.timezone)}
+        <div key={index} className="city-select-item-container">
+          <div
+            className="city-select-item card"
+            onClick={() => handleClick(index)}
+          >
+            <div className="city-select-item-header">
+              <div className="city-select-item-header-left">
+                <div className="city-select-item-name">{weather.name}</div>
+                <div className="city-select-item-time">
+                  {getLocalTime(weather.dt, weather.timezone)}
+                </div>
+              </div>
+              <div className="city-select-item-header-right">
+                <div className="city-select-item-temperature-container">
+                  <div className="city-select-item-temperature">
+                    {weather.main.temp}°C
+                  </div>
+                  <div className="city-select-item-weather-icon">
+                    <img
+                      src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+                      alt="날씨 아이콘"
+                    />
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="city-select-item-header-right">
+            {/* 도시 이름, 온도, 현재 시간, 날씨, 최고, 최저 온도, 날씨 아이콘 */}
+            <div className="city-select-item-content">
+              <div className="city-select-item-weather">
+                {weather.weather[0].description}
+              </div>
               <div className="city-select-item-temperature-container">
-                <div className="city-select-item-temperature">
-                  {weather.main.temp}°C
-                </div>
-                <div className="city-select-item-weather-icon">
-                  <img
-                    src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
-                    alt="날씨 아이콘"
-                  />
-                </div>
+                <span className="city-select-item-max-temperature">
+                  최고 {weather.main.temp_max}°C
+                </span>
+                <span className="city-select-item-min-temperature-separator">
+                  {"  "}
+                </span>
+                <span className="city-select-item-min-temperature">
+                  최저 {weather.main.temp_min}°C
+                </span>
               </div>
             </div>
           </div>
-          {/* 도시 이름, 온도, 현재 시간, 날씨, 최고, 최저 온도, 날씨 아이콘 */}
-          <div className="city-select-item-content">
-            <div className="city-select-item-weather">
-              {weather.weather[0].description}
-            </div>
-            <div className="city-select-item-temperature-container">
-              <span className="city-select-item-max-temperature">
-                최고 {weather.main.temp_max}°C
-              </span>
-              <span className="city-select-item-min-temperature-separator">
-                {"  "}
-              </span>
-              <span className="city-select-item-min-temperature">
-                최저 {weather.main.temp_min}°C
-              </span>
-            </div>
+          <div className="city-select-item-delete">
+            <img
+              src={`public/delete.png`}
+              alt="삭제"
+              onClick={() => handleDelete(index)}
+            />
           </div>
         </div>
       ))}
